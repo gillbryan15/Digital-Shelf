aa37937428f554367edb7f6bcd9e72a5db7675ab
Gill <gillbryan15@gmail.com>
2026-08-23 17:24:52 +0800

    Initial commit: bookshelf project scaffold

---
+++ script.js
@@ -0,0 +1,6 @@
+const titleInput = document.getElementById("book-title");
+const authorInput = document.getElementById("book-author");
+const addBookBtn = document.getElementById("add-book-btn");
+const shelf = document.getElementById("shelf");
+
+let books = []; // this will hold all our book objects
