13433d2674588565f815a2b546a26e10ca29a509
Gill <gillbryan15@gmail.com>
2026-08-25 16:34:06 +0800

    Added localStorage as a temp storage for my shelf

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const authorInput = document.getElementById("book-author");
 const addBookBtn = document.getElementById("add-book-btn");
 const shelf = document.getElementById("shelf");
 const doneCheckbox = document.getElementById("book-done");
-const doneBookBtn = document.getElementById("change-book-status");
+const doneBookBtn = document.getElementById("change-book-status"); 
 
-addBookBtn.addEventListener("click", function(){
-    const title = titleInput.value;
+
+//Initialize all the elements 
+
+addBookBtn.addEventListener("click", function(){  //Button for adding books
+    const title = titleInput.value; //empty box next to button
     const author = authorInput.value;
 
-    if (title === ""){
+    if (title === ""){ //means if nothing in the title box, it doesnt do anything
         return;
     }
 
-    const newBook = {
+    const newBook = { //book object
         title: title,
         author: author,
         color: getRandomColor(),
@@ -22,28 +25,37 @@ addBookBtn.addEventListener("click", function(){
         done: doneCheckbox.checked
     };
 
-    books.push(newBook);
+    books.push(newBook); //adds the book into the book array
 
     renderShelf();
+    saveBooks();
 
     titleInput.value = "";
     authorInput.value = ""; // After input the box will clear
  });
-let books = []; // this will hold all our book objects
+let books = []; // this will hold all our book objects 
 let selectedBook = null; // nothing selected at first
 
-function getRandomColor() {
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
+const saved = localStorage.getItem("myBooks");
+if (saved) {
+  books = JSON.parse(saved);
+}
+
+renderShelf();
+
+function getRandomColor() { 
+  const red = Math.floor(Math.random() * 255); //math random gets a random number between 0-1
+  const green = Math.floor(Math.random() * 255);//math floor rounds up the number to the closest integer
   const blue = Math.floor(Math.random() * 255);
 
   return "rgb(" + red + ", " + green + ", " + blue + ")";
+  
 }
 
 function getRandomHeight() {
     const min = 100;
     const max = 250;
-    const height = Math.floor(Math.random() * (max - min)) + min;
+    const height = Math.floor(Math.random() * (max - min)) + min; //max - min is the range, + min is so that we atleast get the min height.
 
     return height;
 }
@@ -62,17 +74,22 @@ doneBookBtn.addEventListener("click", function() {
   }
   selectedBook.done = !selectedBook.done;
   renderShelf();
+  saveBooks()
 });
+
+function saveBooks() { 
+  localStorage.setItem("myBooks", JSON.stringify(books)); 
+}
 
 //console.log(getRandomHeight()); to check if it runs
 
 function renderShelf() {
   shelf.innerHTML = "";
   
-  books.forEach(function(book) {
-    const bookBlock = document.createElement("div");
-    bookBlock.style.backgroundColor = book.color;
-    bookBlock.style.height = book.height + "px";
+  books.forEach(function(book) { //loops through all books in the array
+    const bookBlock = document.createElement("div"); //Creates a new div for a book
+    bookBlock.style.backgroundColor = book.color; 
+    bookBlock.style.height = book.height + "px"; //adds the details of this specific div
     bookBlock.style.width = book.width + "px";
     bookBlock.title = `${book.title} by ${book.author}`; //tooltip and new way of output
   
@@ -90,11 +107,12 @@ function renderShelf() {
 }
       
     
-    shelf.appendChild(bookBlock);
+    shelf.appendChild(bookBlock); //Places book at the end of the DOM
 
-    bookBlock.addEventListener("click", function (){
+    bookBlock.addEventListener("click", function (){ // this selects books, must be inside the renderShelf function
       selectedBook = book;
       renderShelf();
+      saveBooks();
 
      });
 
